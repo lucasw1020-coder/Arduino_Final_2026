c322ded2eab0207187d2925018391390197154c5
lucasw1020-coder <lucasw1020@icloud.com>
2026-02-26 11:31:37 -0800

    Update Final_Project.ino
    added color / temp controls

--- Final_Project/Final_Project.ino
+++ Final_Project/Final_Project.ino
@@ -2,20 +2,28 @@
 #include <DHT.h>   
 #include <Servo.h>
 
-LiquidCrystal lcd(13, 12, 11, 10, 9, 8, 7); 
+LiquidCrystal lcd(8, 7, 6, 5, 4, 3); 
 
-#define DHTPIN // add DHT pin number here
-#define DHTTYPE DHT11   
-
-DHT dht(DHTPIN, DHTTYPE);
+const int hot_1 = 22;
+const int hot_2 = 28;
+const int warm = 22;
+const int mid_1 = 22;
+const int mid_2 = 32;
+const int cold = 26;
+const int frigid_1 = 26;
+const int frigid_2 = 32;
 
 void setup() {
 
-pinMode( , INPUT); // add DHT pin number here
+pinMode(22, OUTPUT);
+pinMode(24, OUTPUT);
+pinMode(26, OUTPUT);
+pinMode(28, OUTPUT);
+pinMode(30, OUTPUT);
+pinMode(32, OUTPUT);
 
 Serial.begin(9600);
 lcd.begin(16,2);
-lcd.print("Home System Activated");
 lcd.clear();    
 
 }
@@ -23,6 +31,11 @@ lcd.clear();
 void loop() {
 
 lcd.setCursor(0, 0);
-lcd.print("Lucas > Luke LMAO");
+lcd.print("Initializing");
+lcd.setCursor(0, 1);
+lcd.print("Home Control.");
+
+digitalWrite(frigid_2, HIGH);
+digitalWrite(frigid_1, HIGH);
 
 }
