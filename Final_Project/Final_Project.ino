5cea7e63f0f8dbf9589444515e733a148faa85aa
lucasw1020-coder <lucasw1020@icloud.com>
2026-02-26 10:56:24 -0800

    Update Final_Project.ino

--- Final_Project/Final_Project.ino
+++ Final_Project/Final_Project.ino
@@ -1,9 +1,18 @@
 #include <LiquidCrystal.h>
 #include <DHT.h>   
 #include <Servo.h>
+
 LiquidCrystal lcd(13, 12, 11, 10, 9, 8, 7); 
 
+#define DHTPIN // add DHT pin number here
+#define DHTTYPE DHT11   
+
+DHT dht(DHTPIN, DHTTYPE);
+
 void setup() {
+
+pinMode( , INPUT); // add DHT pin number here
+
 Serial.begin(9600);
 lcd.begin(16,2);
 lcd.print("Home System Activated");
