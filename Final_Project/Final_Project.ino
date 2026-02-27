5c6004c0e4709c222e6f16af18b6cdda6aa68d2f
lucasw1020-coder <lucasw1020@icloud.com>
2026-02-27 11:09:12 -0800

    Update Final_Project.ino
    got leds working with the potentiometer

--- Final_Project/Final_Project.ino
+++ Final_Project/Final_Project.ino
@@ -7,13 +7,13 @@ LiquidCrystal lcd(8, 7, 6, 5, 4, 3);
 const int hot_1 = 22;
 const int hot_2 = 28;
 const int warm = 22;
-const int mid_1 = 22;
-const int mid_2 = 32;
-const int cold = 26;
+const int mid_1 = 32;
+const int mid_2 = 22;
+const int cold = 32;
 const int frigid_1 = 26;
 const int frigid_2 = 32;
 
-int wait = analogRead(A_); // add port
+int lastSection = -1;
 
 void setup() {
 
@@ -39,5 +39,71 @@ void loop() {
 
 lcd.setCursor(0, 0);
 
+int color_potValue = analogRead(A0);
 
+int section;
+
+if (color_potValue > 670) 
+{
+  section = 4;
+}
+else {
+  section = color_potValue * 5 / 1024;
+}  
+
+Serial.println(section);
+Serial.println(color_potValue);
+  if(section != lastSection) {
+
+    // Turn everything off ONCE
+    digitalWrite(22, LOW);
+    digitalWrite(24, LOW);
+    digitalWrite(26, LOW);
+    digitalWrite(28, LOW);
+    digitalWrite(30, LOW);
+    digitalWrite(32, LOW);
+
+switch(section) {
+
+case 0:  // 2 blue
+digitalWrite(frigid_2, HIGH);
+digitalWrite(frigid_1, HIGH);
+
+break;
+
+case 1: // 1  blue
+digitalWrite(cold, HIGH);
+digitalWrite(26, LOW);
+
+break;
+
+case 2: //mid
+digitalWrite(mid_1, HIGH);
+digitalWrite(mid_2, HIGH);
+
+break;
+
+case 3: //warm
+digitalWrite(warm, HIGH);
+digitalWrite(30, LOW);
+
+break;
+
+case 4: //hot
+digitalWrite(hot_1, HIGH);
+digitalWrite(hot_2, HIGH);
+
+break;
+
+default: 
+  digitalWrite(22, LOW);
+  digitalWrite(24, LOW);
+  digitalWrite(26, LOW);
+  digitalWrite(28, LOW);
+  digitalWrite(30, LOW);
+  digitalWrite(32, LOW);
+}
+
+ lastSection = section;
+  }
 }
